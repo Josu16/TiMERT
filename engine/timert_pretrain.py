a2f3c78aef0572ae21e140116a4ef8b5729240fc
Josue <josuloop@icloud.com>
2024-08-17 00:55:27 +0200

    TFM-17 feat: encoder previous MLFlow implementation

--- engine/timert_pretrain.py
+++ engine/timert_pretrain.py
@@ -1,4 +1,3 @@
-## RoBERTa approach
 import os
 import torch
 import time
@@ -12,33 +11,30 @@ from transformers import RobertaConfig, RobertaModel, Adafactor
 
 from engine.core.ts_transformer import Transformer
 from engine.core.timert_utils import _normalize_dataset, get_dataset, get_ucr_dataset_names, mask_data
-from engine.data_models.global_params_model import TimertGlobalParams
-from engine.data_models.pretrain_params_model import TimertPreTrainParams
 
 
 class TimertPreTrain:
     
-    def __init__(
-            self, 
-            model_params: TimertPreTrainParams, 
-            global_params: TimertGlobalParams, 
-            gpu_id: str
-        ):
+    def __init__(self, params, gpu_id: str):
 
-        self.model_params = model_params
+        self.global_params = params["global"]
+        self.prep_params = params["preprocess"]
+        self.model_params = params["model"]
+        self.pretext_params = params["pretext_task"]
+        self.train_params = params["train"]
 
         ## Initial settings
         os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         print(f'Using device: {self.device}')
-        np.random.seed(global_params.seed)
+        np.random.seed(self.global_params["seed"])
 
 
         # Split dataset
         data_names = get_ucr_dataset_names()
 
         total_datasets = len(data_names)
-        pretrain_size = int(total_datasets * global_params.pretrain_frac)
+        pretrain_size = int(total_datasets * self.prep_params["pretrain_frac"])
 
         index = np.random.permutation(total_datasets)
 
@@ -62,7 +58,7 @@ class TimertPreTrain:
         pretrain_data = []
         for data_name in self.pretrain_names:
             print("Processing dataset: ", data_name)
-            dataset, _ = get_dataset("UCRArchive_2018", data_name, max_len=self.model_params.out_dim)
+            dataset, _ = get_dataset("UCRArchive_2018", data_name, max_len=self.model_params["out_dim"])
             data_pretrain = _normalize_dataset(dataset)
 
             # fasjfsfjsdfkasdj
@@ -75,22 +71,22 @@ class TimertPreTrain:
 
 
     def start_pretrain(self):
-        batch_size = self.model_params.batch_size  # Definir el tamaño del batch
+        batch_size = self.train_params["batch_size"]  # Definir el tamaño del batch
         dataset = TensorDataset(self.pretrain_data)
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
         # Definir el modelo
         model = Transformer(
-            in_dim = self.model_params.in_dim,
-            out_dim = self.model_params.out_dim,
-            n_layer = self.model_params.n_layer,
-            n_dim = self.model_params.n_dim,
-            n_head = self.model_params.n_head,
-            norm_first = self.model_params.norm_first,
-            is_pos = self.model_params.is_pos,
-            is_projector = self.model_params.is_projector,
-            project_norm = self.model_params.project_norm,
-            dropout = self.model_params.dropout
+            in_dim = self.model_params["in_dim"],
+            out_dim = self.model_params["out_dim"],
+            n_layer = self.model_params["n_layer"],
+            n_dim = self.model_params["n_dim"],
+            n_head = self.model_params["n_head"],
+            norm_first = self.model_params["norm_first"],
+            is_pos = self.model_params["is_pos"],
+            is_projector = self.model_params["is_projector"],
+            project_norm = self.model_params["project_norm"],
+            dropout = self.model_params["dropout"]
         ).to(self.device)
 
         print("Modelo inicializado:")
@@ -99,19 +95,20 @@ class TimertPreTrain:
 
         # Definir la función de pérdida y el optimizador
         criterion = nn.MSELoss()  # Por ejemplo, MSE para series temporales
-        optimizer = optim.Adam(model.parameters(), lr=self.model_params.lr)
+        optimizer = optim.Adam(model.parameters(), lr=self.train_params["lr"])
 
         # Iniciar el contador total de tiempo
         total_start_time = time.time()
 
         # Entrenamiento
-        for epoch in range(self.model_params.n_epoch):  # Número de épocas
+        n_epoch = self.train_params["n_epoch"]
+        for epoch in range(n_epoch):  # Número de épocas
             start_time = time.time()
             total_loss = 0.0
 
             # Usamos tqdm para mostrar el progreso
             with tqdm(dataloader, unit="batch") as tepoch:
-                tepoch.set_description(f"Epoch {epoch+1}/{self.model_params.n_epoch}")
+                tepoch.set_description(f"Epoch {epoch+1}/{n_epoch}")
                 for batch in tepoch:
                     masked_data, mask = mask_data(batch[0])  # Enmascarar datos
                     optimizer.zero_grad()
--- timert_cli.py
+++ timert_cli.py
@@ -1,52 +1,26 @@
 import typer
+import yaml
 
 from engine.timert_pretrain import TimertPreTrain
-from engine.data_models.pretrain_params_model import TimertPreTrainParams
-from engine.data_models.global_params_model import TimertGlobalParams
 
 app = typer.Typer()
 
 
 @app.command()
 def pretrain(
+    conf_file: str = typer.Option(..., help="The configuration file name"),
     gpu_id: str = typer.Option("0", help="The GPU ID to use")
 ):
+    print(f"Selected File: {conf_file}")
     print(f"Selected GPU: {gpu_id}")
-    
-    global_params = TimertGlobalParams(
-        data_dir = "UCRArchive_2018/",
-        max_len = 512,
-        seed = 666,
-        pretrain_frac = 0.65,
-        train_frac = 0.3,
-        valid_frac = 0.1,
-        test_frac = 0.1,
-        is_same_length = True
-        )
 
-    model_params = TimertPreTrainParams(
-        in_dim = 1,
-        out_dim = 512,
-        n_layer = 4,
-        n_dim = 64,
-        n_head = 8,
-        norm_first = True,
-        is_pos = True,
-        is_projector = True,
-        project_norm = "LN",
-        dropout = 0.0,
-        lr = 0.0001,
-        batch_size = 128,
-        n_epoch = 150,
-        n_ckpt = 100,
-        )
+    with open(f'parameters/{conf_file}.yml', 'r') as file:
+        all_params = yaml.safe_load(file)
 
-    timer_model = TimertPreTrain(
-        model_params=model_params,
-        global_params=global_params,
-        gpu_id=gpu_id
-        )
+    print(type(all_params))
+    print(all_params)
 
+    timer_model = TimertPreTrain(params = all_params, gpu_id=gpu_id)
     timer_model.preprocessing_time_series()
     timer_model.start_pretrain()
 
